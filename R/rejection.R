a07ec2e91587c1ba18e13a8d1a0c1f4e130b474e
Jacob Carey <jacobcvt12@gmail.com>
2015-08-28 21:33:56 -0400

    Correct rejection sampler.

--- R/rejection.R
+++ R/rejection.R
@@ -1,23 +1,22 @@
 abc_rejection <- function(data, iter=1e3, eps=1e-1) {
-
     size <- length(data)
-    data_summary <- c(mean(data), sd(sample))
+    data_summary <- c(mean(data), sd(data))
 
     # allocate space
-    theta <- matrix(NA, n, 2)
+    theta <- matrix(NA, iter, 2)
 
     i <- 1
 
     for (s in 1:iter) {
-        shape <- runif(n, 1, 6)
-        scale <- runif(n, 1, 10)
+        shape <- runif(1, 1, 6)
+        scale <- runif(1, 1, 10)
 
-        sample <- rweibull(n, shape, scale)
+        sample <- rweibull(size, shape, scale)
         sample_summary <- c(mean(sample), sd(sample))
 
         deviance <- sqrt(sum((sample_summary - data_summary) ^ 2))
 
-        if (deviance < abs) {
+        if (deviance < eps) {
             theta[i, ] <- c(shape, scale)
 
             i <- i + 1
